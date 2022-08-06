d3ab7e7f2aa5ff7c8ab30b85eebf71bcb005fada
AzureAD\KarolinaKowalska <karolina.kowalska@summ-it.pl>
2022-08-06 18:02:37 +0200

    CQRS + CRUD handlers + Adding AutoMapper + Program class housekeeping

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Application.Activties;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen();
+
+            // TODO: Db service providers with migration
+
+            #region Register Containers
+
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            #endregion
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,26 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// TODO: Db service providers with migration
-
-#region Register Containers
-
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-#endregion
-
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
