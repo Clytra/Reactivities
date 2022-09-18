04cfd828c4d8f050162aa37cfb4803255e60c407
AzureAD\KarolinaKowalska <karolina.kowalska@summ-it.pl>
2022-09-18 19:10:29 +0200

    Handling exceptions

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Application.Activties;
 using FluentValidation.AspNetCore;
 
@@ -18,10 +19,11 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
